6288c5b059e34d23331bd08fcc24f76d2e3a3ae5
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-05-12 18:06:21 +0200

    Update OdooRPC Version

--- api/config.py
+++ api/config.py
@@ -3,6 +3,6 @@ ODOO_RPC = {
     'host': '192.168.0.223',
     'port': 8069,
     'login': 'alejperez99@hotmail.com',
-    'password': 'ale1234510',
-    'database': 'NoDemoDB'
+    'password': 'odoo',
+    'database': 'NoDemoDB1'
 }
